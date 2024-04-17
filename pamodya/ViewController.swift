61a8f6390c14dad022eb69260ac68c3e19c92d0d
IT20664558 D.M.P.D.Daundasekara <80777419+Pamdil@users.noreply.github.com>
2024-04-17 21:37:18 +0530

    initiate Repo

---
+++ pamodya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pamodya
+//
+//  Created by Isiwara Uditha on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
